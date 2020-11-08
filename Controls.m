0e4e1a48f31fa48271f954545516eb50b9ce6533
p4trucc0 <patrucco@outlook.it>
2020-11-08 15:26:42 +0100

    Added steering wheel to future-proof the Controls class.

--- Controls.m
+++ Controls.m
@@ -7,15 +7,21 @@ classdef Controls < handle
         brk_pedal = 0.0; % brake (0 - 1.0)
         clc_pedal = 0.0; % clutch (0 - 1.0)
         gear_lever = 0; % gear lever selection
+        ste_wheel = 0.0; % steering wheel (-1, 1)
     end
     
     methods
         
-        function set_all(obj, n_gas_pedal, n_brk_pedal, n_clc_pedal, n_gear_lever)
+        function set_all(obj, n_gas_pedal, n_brk_pedal, n_clc_pedal, n_gear_lever, n_ste_wheel)
             obj.gas_pedal = n_gas_pedal;
             obj.brk_pedal = n_brk_pedal;
             obj.clc_pedal = n_clc_pedal;
             obj.gear_lever = n_gear_lever;
+            if nargin > 5
+                obj.ste_wheel = n_ste_wheel;
+            else
+                obj.ste_wheel = 0.0;
+            end
         end
         
         
